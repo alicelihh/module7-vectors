712769b3dabae98cf5dc1afe99498fd04556e8a0
Alice Li <alicehh@uw.edu>
2017-01-12 14:55:01 -0800

    Finished the practice questions

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,13 +1,13 @@
 # Exercise 1: Creating and Indexing Vectors
 
 # Create a vector `x` that has the values 10 through 20 in it (using the : operator)
-
+x <- c(10:20)
 
 # Create a vector `y` that has the values 21 through 30 in it (using the seq() function)
-
+y <- seq(21, 30)
 
 # Create a vector `z` by combining the vectors `x` and `y`
-
+z <- combn(x, y)
 
 # Create a variable `w` that contains the 11th element in your vector `z`
 
