562203bb3ff72a3b3d9df6f0e0817828699846da
Alice Li <alicehh@uw.edu>
2017-01-16 16:19:50 -0800

    Completed exercise-1

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -7,21 +7,27 @@ x <- c(10:20)
 y <- seq(21, 30)
 
 # Create a vector `z` by combining the vectors `x` and `y`
-z <- combn(x, y)
-
+z <- c(x, y)
+print(z)
 # Create a variable `w` that contains the 11th element in your vector `z`
-
+w <- z[11]
 
 # Create a vector `q` that contains the second through the 5 elements of `z`
-
+q <- z[2:5]
 
 # Create a variable `len` that is equal to the length of your vector `q`
-
+len <- length(q)
 
 ### Bonus ###
 
 # Create a vector `even` that holds the even numbers from 1 to 100
-
+original.numbers <- c(1:100)
+original.number.check <- original.numbers / 2
+round.number.check <- round(original.number.check)
+indices <- round.number.check == original.number.check
+even <- original.numbers[indices]
+print(even)
 
 # Using the `all()` function and `%%` (modulo) operator, confirm that all of the
 # numbers in your `even` vector are even
+all(even %% even)
